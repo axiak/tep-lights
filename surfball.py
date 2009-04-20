31bd963fb6bdbe3df24874d7ffeb2a209dfd326c
Matthew Peddie <peddie@alqamar.(none)>
2009-04-20 06:07:18 -0400

    simple flames from the bottom of the panel

--- surfball.py
+++ surfball.py
@@ -1,14 +1,24 @@
 import dmx
 import math
 
+
+
 panel = dmx.LightPanel("18.224.3.100", 6038, 0)
 
 def colorset(panel, hue, brightness, x, y):
     panel.lights[int(y)][int(x)].sethue(hue, brightness, 0)
 
-def setColor(panel, hue, x, y, row, col) :
-    dist = math.sqrt((x - col)**2 + (y - row)**2)
-    panel.lights[row][col].sethue(hue/(1 + 0.4*dist), 2/(0.05 + dist), 0)
+def setColor(panel, hue, x, y, row, col):
+    hs = 3               # hue variance
+    bs = 4               # brightness variance
+    ss = 3               # saturation variance
+    dist = math.sqrt((x - col)**2. + (y - row)**2)
+    #panel.lights[row][col].sethue(hue/(1 + 0.4*dist), 2/(0.05 + dist), 0)
+    pointhue = 2*hs*math.exp(-dist**2/(2*hs**2))/(hs*math.sqrt(2*math.pi))
+    pointbrightness = 3*bs*math.exp(-dist**2/(2*bs**2))/(bs*math.sqrt(2*math.pi))
+    pointsat = 1/(1+dist**2)
+    #pointsat = ss*math.exp(-dist**2/(2*ss**2))/(ss*math.sqrt(2*math.pi))
+    panel.lights[row][col].sethue(pointhue, pointbrightness, pointsat)
 
 def clear(panel):
     for row in panel.lights:
