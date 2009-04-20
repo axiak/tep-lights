3f15869a99715efb1fc62bc70bebc0ce5eb05d51
Matthew Peddie <peddie@alqamar.(none)>
2009-04-20 07:59:54 -0400

    quick clock updates so it is more accurate and has hand hiding

--- clock.py
+++ clock.py
@@ -14,26 +14,29 @@ centerx = (panel.width-1)/2.
 centery = (panel.height-1)/2.
 
 def drawClock(panel, hours, mins, secs):
+    h = (hours + 1/60.*mins)%12
+    m = mins + 1/60*secs
     for x in xrange(panel.width):
         for y in xrange(panel.height):
             #rim
             if math.sqrt((x-centerx)**2 + (y-centery)**2) >= min(panel.width, panel.height)/2.:
-                panel.lights[x][y].sethue(rimhue, 0.5, 0)
+                panel.lights[x][y].sethue(rimhue, 0.5, 0) 
+            #hours
+            elif math.fabs(math.degrees((math.atan2(centerx-x, y-centery))) + 90 - 30*h) <= 7:
+                panel.lights[x][y].sethue(hourshue, 1, 0)
+            elif math.fabs(math.degrees((math.atan2(x-centerx, centery-y))) + 270 - 30*h) <= 7:
+                panel.lights[x][y].sethue(hourshue, 1, 0)
+            #minutes
+            elif math.fabs(math.degrees((math.atan2(centerx-x, y-centery))) + 90 - 6*m) <= 7:
+                panel.lights[x][y].sethue(minuteshue, 1, 0)
+            elif math.fabs(math.degrees((math.atan2(x-centerx, centery-y))) + 270 - 6*m) <= 7:
+                panel.lights[x][y].sethue(minuteshue, 1, 0)
             #seconds
             elif math.fabs(math.degrees((math.atan2(centerx-x, y-centery))) + 90 - 6*secs) <= 7:
                 panel.lights[x][y].sethue(secondshue, 1, 0)
             elif math.fabs(math.degrees((math.atan2(x-centerx, centery-y))) + 270 - 6*secs) <= 7:
                 panel.lights[x][y].sethue(secondshue, 1, 0)
-            #minutes
-            elif math.fabs(math.degrees((math.atan2(centerx-x, y-centery))) + 90 - 6*mins) <= 7:
-                panel.lights[x][y].sethue(minuteshue, 1, 0)
-            elif math.fabs(math.degrees((math.atan2(x-centerx, centery-y))) + 270 - 6*mins) <= 7:
-                panel.lights[x][y].sethue(minuteshue, 1, 0)
-            #hours
-            elif math.fabs(math.degrees((math.atan2(centerx-x, y-centery))) + 90 - 15*hours) <= 7:
-                panel.lights[x][y].sethue(hourshue, 1, 0)
-            elif math.fabs(math.degrees((math.atan2(x-centerx, centery-y))) + 270 - 15*hours) <= 7:
-                panel.lights[x][y].sethue(hourshue, 1, 0)
+            #center blob
             elif math.fabs(x - centerx) < 1 and math.fabs(y - centery) < 1:
                 panel.lights[x][y].sethue(rimhue, 0.75, 0)
             else:
@@ -42,6 +45,5 @@ def drawClock(panel, hours, mins, secs):
             
 while True :
     t = datetime.datetime.now().timetuple()
-    print "===", t[5], "==="
     drawClock(panel, t[3], t[4], t[5])
-    panel.outputAndWait(10)
+    panel.outputAndWait(3)
