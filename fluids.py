6fdb713bc9e6c4d5e6f70ca3f4687a47334291d0
Kyle Miller <kyle@kmill.mit.edu>
2009-05-20 14:31:41 -0400

    fluids

--- fluids.py
+++ fluids.py
@@ -32,30 +32,44 @@ class Fluids (dmxwidget.Widget) :
     def init(self, panel) :
         self.heights = [[0.0 for col in row] for row in panel.lights]
         self.veloc = [[0.0 for col in row] for row in panel.lights]
-        self.heights[5][5] = 2.0
+        #self.heights[5][5] = 2.0
+        for row in xrange(0, panel.height-0) :
+            for col in xrange(0, panel.width-0) :
+                #self.heights[row][col] = math.sin(4*row*2*math.pi/panel.height)
+                #self.veloc[row][col] = math.cos(4*row*2*math.pi/panel.height)
+                pass
         self.boardwidth = 2.0
         self.boardheight = 2.0
     def f(self, x, y, theta) :
         return math.cos(10*x*x*math.cos(0.1*theta) + 10*y*y*math.sin(0.1*theta)+math.cos(0.011*theta)*x*y)
     def updateFluid(self) :
         dampen=0.005 # higher is more
-        c2 = 0.05
-        dt = 0.05
+        #dampen = 0
+        c2 = 0.07
+        dt = 0.03
         h = self.boardwidth/len(self.heights)
         height = len(self.veloc)
         width = len(self.veloc[0])
         if random.random() < 0.008 :
-            amount = 4*random.random()-2.0
+            amount = 2.0*random.random()
             locrow = random.randrange(0, height)
             loccol = random.randrange(0, width)
-            self.heights[locrow][loccol] += amount
-            self.veloc[locrow][loccol] += amount
+            for row in xrange(-10, 11) :
+                for col in xrange(-10, 11) :
+                    arow = (row+locrow)%height
+                    acol = (col+loccol)%width
+                    a2row = (row+4+locrow)%height
+                    a2col = (col+4+loccol)%width
+                    #self.heights[arow][acol] -= 3*amount/2**(math.sqrt((row)**2+(col)**2))
+                    self.veloc[a2row][a2col] -= 20*amount/2**(math.sqrt((row)**2+(col)**2))
         for row in xrange(height) :
             for col in xrange(width) :
                 drow2 = (self.heights[(row+1)%height][col]-2*self.heights[row][col]+self.heights[(row-1)%height][col])/h**2
                 dcol2 = (self.heights[row][(col+1)%width]-2*self.heights[row][col]+self.heights[row][(col-1)%width])/h**2
                 df2 = c2 * (drow2+dcol2)
                 self.veloc[row][col] += df2*dt - self.veloc[row][col]*dampen
+                #if row==0 or row==height-1 or col==0 or col==width-1 :
+                    #self.veloc[row][col] = 0
         for row in xrange(height) :
             for col in xrange(width) :
                 self.heights[row][col] += self.veloc[row][col] * dt
