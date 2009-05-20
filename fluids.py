9296588e728d0f825e970dc2a5ef5dab1232390f
Kyle Miller <kyle@kmill.mit.edu>
2009-05-20 11:27:18 -0400

    fluids

--- fluids.py
+++ fluids.py
@@ -32,24 +32,24 @@ class Fluids (dmxwidget.Widget) :
     def init(self, panel) :
         self.heights = [[0.0 for col in row] for row in panel.lights]
         self.veloc = [[0.0 for col in row] for row in panel.lights]
-        self.heights[5][5] = 1.0
-        self.heights[5][4] = 0.5
-        self.heights[5][6] = 0.5
-        self.heights[4][5] = 0.5
-        self.heights[6][4] = 0.5
+        self.heights[5][5] = 2.0
         self.boardwidth = 2.0
         self.boardheight = 2.0
     def f(self, x, y, theta) :
         return math.cos(10*x*x*math.cos(0.1*theta) + 10*y*y*math.sin(0.1*theta)+math.cos(0.011*theta)*x*y)
     def updateFluid(self) :
-        dampen=0.009 # higher is more
-        c2 = 0.1
+        dampen=0.005 # higher is more
+        c2 = 0.05
         dt = 0.05
         h = self.boardwidth/len(self.heights)
         height = len(self.veloc)
         width = len(self.veloc[0])
         if random.random() < 0.008 :
-            self.heights[random.randrange(0, height)][random.randrange(0, width)] += 2*random.random()-1
+            amount = 4*random.random()-2.0
+            locrow = random.randrange(0, height)
+            loccol = random.randrange(0, width)
+            self.heights[locrow][loccol] += amount
+            self.veloc[locrow][loccol] += amount
         for row in xrange(height) :
             for col in xrange(width) :
                 drow2 = (self.heights[(row+1)%height][col]-2*self.heights[row][col]+self.heights[(row-1)%height][col])/h**2
@@ -72,11 +72,11 @@ class Fluids (dmxwidget.Widget) :
         h = 2.0/self.boardwidth
         for r in range(0, panel.height) :
             for c in range(0, panel.width) :
-                dx = (self.heights[r][(c+1)%panel.width]-self.heights[r][c])/h
-                dy = (self.heights[(r+1)%panel.height][c]-self.heights[r][c])/h
+                dx = (self.heights[r][(c+1)%panel.width]-self.heights[r][c])
+                dy = (self.heights[(r+1)%panel.height][c]-self.heights[r][c])
                 intens = (-dx*sunx - dy*suny + sunz)/math.sqrt(1 + dx*dx + dy*dy)
                 intens = (1+intens)/2
-                panel.lights[r][c].sethue(0.8, intens, max(0.0,4.0*intens-3.0))
+                panel.lights[r][c].sethue(0.8, intens**0.5, max(0.0,4.0*intens-3.0))
         panel.outputAndWait(30)
 
 if __name__=="__main__" :
