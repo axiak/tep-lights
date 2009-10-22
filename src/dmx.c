bfff3617135f34fce60bedae18bbc1c2091987c9
axiak <axiak@c46fabcb-0fd2-43a4-8a37-601b2e6a48eb>
2009-10-22 04:17:20 +0000

    Added code.
    git-svn-id: svn+ssh://linerva.mit.edu/mit/axiak/svnrepos/public/tEp/cdmx@118 c46fabcb-0fd2-43a4-8a37-601b2e6a48eb

---
+++ src/dmx.c
@@ -0,0 +1,232 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+#include "dmx.h"
+
+int _dmxpanel_createsocket(DMXPanel * panel);
+static inline int _dmxpanel_senddata(DMXPanel * panel, char * output, int len);
+
+static inline void _ERROR(char * s) {
+    fprintf(stderr, "%s\n", s);
+    exit(2);
+}
+
+/* Default map function */
+int ledmap_default(int r, int c)
+{
+    return r + 12 * (5 - c);
+}
+
+
+/* RGBLed stuff */
+RGBLed * pixel_setrgb(RGBLed * led, unsigned char red, unsigned char green, unsigned char blue)
+{
+    led->red = red;
+    led->green = green;
+    led->blue = blue;
+    return led;
+}
+
+RGBLed * pixel_sethue(RGBLed * led, float hue, float brightness, float saturation)
+/* Great "hsv" algorithm from kmill... */
+{
+    hue *= 6;
+    float angle = ((int)hue % 6) + (hue - (int)hue);
+
+    brightness = MIN(MAX(brightness, 0), 1);
+    saturation = MIN(MAX(saturation, 0), 1);
+
+    if (angle < 2) {
+        led->red = 1;
+        if (angle < 1) {
+            led->green = 0;
+            led->blue = 1 - angle;
+        }
+        else {
+            led->green = angle - 1;
+            led->blue = 0;
+        }
+    }
+    if (angle >= 2 && angle < 4) {
+        led->green = 1;
+        if (angle < 3) {
+            led->red = 3 - angle;
+            led->blue = 0;
+        }
+        else {
+            led->red = 0;
+            led->blue = angle - 3;
+        }
+    }
+    if (angle >= 4) {
+        led->blue = 1;
+        if (angle < 5) {
+            led->green = 5 - angle;
+            led->red = 0;
+        }
+        else {
+            led->green = 0;
+            led->red = angle - 5;
+        }
+    }
+
+    led->red = brightness * (MIN(MAX(brightness - saturation, 0.0), 1.0) *
+                             led->red + saturation);
+    led->green = brightness * (MIN(MAX(brightness - saturation, 0.0), 1.0) *
+                             led->green + saturation);
+    led->blue = brightness * (MIN(MAX(brightness - saturation, 0.0), 1.0) *
+                             led->blue + saturation);
+    return led;
+}
+
+RGBLed * pixel_add(RGBLed * dst, RGBLed * src)
+{
+    dst->red = MAX(dst->red + src->red, 1.0);
+    dst->green = MAX(dst->green + src->green, 1.0);
+    dst->blue += MAX(dst->blue + src->blue, 1.0);
+    return dst;
+}
+
+RGBLed * pixel_multiply(RGBLed * dst, RGBLed * src)
+{
+    dst->red *= src->red;
+    dst->green *= src->green;
+    dst->blue *= src->blue;
+    return dst;
+}
+
+
+
+/* LEDArray stuff */
+LEDArray * ledarray_create(SZ size)
+{
+    LEDArray * la;
+    la = (LEDArray *)malloc(sizeof(LEDArray));
+    if (!la) {
+        _ERROR("Could not get memory for LEDArray!");
+    }
+    la->size = size;
+    la->led = (RGBLed *)calloc(size, sizeof(RGBLed));
+    if (!la->led) {
+        _ERROR("Could not get memory for LEDArray!");
+    }
+    return la;
+}
+
+void ledarray_destroy(LEDArray * ledarray)
+{
+    if (!ledarray)
+        return;
+    if (ledarray->led) {
+        free(ledarray->led);
+    }
+    free(ledarray);
+}
+
+
+
+/* DMX Stuff */
+DMXPanel * dmxpanel_create(char * ip, unsigned short port, SZ width, SZ height, ledmap * mapfunc)
+{
+    DMXPanel * panel = (DMXPanel *)malloc(sizeof(DMXPanel));
+    if (!panel) {
+        _ERROR("Could not allocate memory for panel");
+    }
+    panel->ip = ip;
+    panel->width = width;
+    panel->height = height;
+    panel->port = port;
+    if (mapfunc) {
+        panel->func = mapfunc;
+    }
+    else {
+        panel->func = (ledmap *)ledmap_default;
+    }
+
+    panel->leds = ledarray_create(width * height);
+
+    panel->sockfd = -1;
+    if (_dmxpanel_createsocket(panel)) {
+        _ERROR("Could not create socket");
+    }
+    return panel;
+}
+
+
+void dmxpanel_destroy(DMXPanel * panel)
+{
+    if (!panel) {
+        return;
+    }
+    if (panel->leds) {
+        ledarray_destroy(panel->leds);
+    }
+    free(panel);
+}
+
+
+int _dmxpanel_createsocket(DMXPanel * panel)
+{
+    int sockfd;
+    struct sockaddr_in * server_addr = (struct sockaddr_in *)malloc(sizeof(struct sockaddr_in));
+    struct hostent *server;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        _ERROR("Could not open socket");
+    }
+
+    server = gethostbyname(panel->ip);
+    if (server == NULL) {
+        _ERROR("No such hostname");
+    }
+
+    server_addr->sin_family = AF_INET;
+    server_addr->sin_port = htons(panel->port);
+    server_addr->sin_addr = *((struct in_addr *)server->h_addr_list[0]);
+    memset(&(server_addr->sin_zero), 0, 8);
+
+    panel->sockfd = sockfd;
+    panel->server_addr = (struct sockaddr *)server_addr;
+    return 0;
+}
+
+static inline int _dmxpanel_senddata(DMXPanel * panel, char * output, int len)
+{
+    return sendto(panel->sockfd, output, len, 0, panel->server_addr, sizeof(struct sockaddr));
+}
+
+void dmxpanel_sendframe(DMXPanel * panel)
+{
+    char * buffer;
+    buffer = (char *)malloc(23 + 55);
+    
+}
+
+
+
+extern inline RGBLed * dmxpanel_getpixel(DMXPanel * dmxpanel, SZ x, SZ y)
+{
+    unsigned int idx;
+    idx = (*(dmxpanel->func))(x, y);
+    if (idx < 0 || idx >= dmxpanel->leds->size) {
+        _ERROR("Invalid index for leds!");
+    }
+    return &(dmxpanel->leds->led[idx]);
+}
+
+
+
+#ifdef DMX_TEST
+int main(int argc, char ** argv)
+{
+    printf("TEST %s\n", "\x41");
+    return 0;
+}
+#endif
--- src/dmx.h
+++ src/dmx.h
@@ -1,32 +1,42 @@
 #ifndef __DMX_H
 #define __DMX_H
+#include <sys/socket.h>
 
 #define SZ unsigned int
 
 /* Structures for controlling DMX panels */
-
 typedef struct {
-  unsigned char red;
-  unsigned char green;
-  unsigned char blue;
+    float red;
+    float green;
+    float blue;
 } RGBLed;
 
 typedef struct {
-  RGBLed * led;
-  SZ size;
+    RGBLed * led;
+    SZ size;
 } LEDArray;
 
+
+/* LED Mapping function */
+typedef int (*ledmap)(int, int);
+
 typedef struct {
-  char * ip;
-  SZ width;
-  SZ height;
-  int sockfd;
-  LEDArray * leds;
+    char * ip;
+    unsigned short port;
+    SZ width;
+    SZ height;
+    ledmap * func;
+    LEDArray * leds;
+    int sockfd;
+    struct sockaddr * server_addr;
 } DMXPanel;
+
 
 /* Initializers */
 LEDArray * ledarray_create(SZ size);
-DMXPanel * dmxpanel_create(char * ip, SZ width, SZ height);
+
+/* If mapfunc is null, then the default of (r + 12*(5 - c)) */
+DMXPanel * dmxpanel_create(char * ip, unsigned short port, SZ width, SZ height, ledmap * mapfunc);
 
 
 /* Destroyers */
@@ -34,10 +44,25 @@ void ledarray_destroy(LEDArray * ledarray);
 void dmxpanel_destroy(DMXPanel * dmxpanel);
 
 /* Other functions */
+void dmxpanel_sendframe(DMXPanel * panel);
 extern inline RGBLed * dmxpanel_getpixel(DMXPanel * dmxpanel, SZ x, SZ y);
 
-void pixel_setrgb(unsigned char red, unsigned char green, unsigned char blue);
-void pixel_sethsv(un
+RGBLed * pixel_setrgb(RGBLed * led, unsigned char red, unsigned char green, unsigned char blue);
+RGBLed * pixel_sethue(RGBLed * led, float hue, float brightness, float saturation);
+
+/* These operate such that the destination is dst.
+   I.e.: dst <- dst * src
+*/
+RGBLed * pixel_add(RGBLed * dst, RGBLed * src);
+RGBLed * pixel_multiply(RGBLed * dst, RGBLed * src);
+
+#ifndef MIN
+#define MIN(a, b) ((a) < (b)) ? (a) : (b)
+#endif
+#ifndef MAX
+#define MAX(a, b) ((a) > (b)) ? (a) : (b)
+#endif
+
 
 
 #endif
