d8737fdc151c019b90dffba55905f7d6146cb706
kmill <kmill@c46fabcb-0fd2-43a4-8a37-601b2e6a48eb>
2009-10-24 02:58:22 +0000

    plugins
    git-svn-id: svn+ssh://linerva.mit.edu/mit/axiak/svnrepos/public/tEp/cdmx@170 c46fabcb-0fd2-43a4-8a37-601b2e6a48eb

--- plugins/circle.c
+++ plugins/circle.c
@@ -4,7 +4,7 @@
 #include <ipcstructs.h>
 #include <dmx.h>
 
-#define PLUGINID 301
+#define PLUGINID 303
 
 int main(int argc, char **argv)
 {
@@ -17,33 +17,21 @@ int main(int argc, char **argv)
     i = 0;
     layer->width = 48;
     layer->height = 24;
+    float avg_vol = 0;
     while (1) {
         serverdata_update(s); /* Wait for audio info to update */
-        printf("YO\n");
-        /* Get other plugin information */
-
-       /*  for (j = 0; j < MAXPLUGINS; j++) { */
-/*             if (s->info->input_plugins[j]) { */
-/*                 /\* The plugin is one of its inputs, we should use it now. *\/ */
-/*                 layer2 = plugin_useotherlayer(s->ipcdata, j); */
-/*                 /\* do stuff with layer2...*\/ */
-/*                 colorlayer_add(layer, layer2); */
-/*                 plugin_disuseotherlayer(s->ipcdata, j); */
-/*             } */
-/* 	} */
-
-
         
         /* Do stuff to layer... */
-        rgbpixel_sethbsvalue(&color, i/48.0, 1.0, 0.0, 1.0);
-        rgbpixel_setvalue(&color2, 0, 1.0, 0, 1.0);
+	//        rgbpixel_sethbsvalue(&color, i/48.0, 1.0, 0.0, 1.0);
+	//        rgbpixel_setvalue(&color2, 0, 1.0, 0, 1.0);
 
-	//colorlayer_setall(layer, 0, 0, 0, 0);
+	colorlayer_setall(layer, 0, 0, 0, 0);
 	
-        r = i / 48;
-        c = i % 48;
+	avg_vol = (30*avg_vol + s->soundinfo->volumehistory[0])/31.0;
+	float dvol = (s->soundinfo->volumehistory[0]-avg_vol)/avg_vol;
 
-        draw_circle(layer, rand()%48, rand()%24, rand()%30, &color);
+        draw_circle(layer, 24, 12, dvol+10, &RED);
+        draw_circle(layer, 24, 12, rand()%10, &RED);
 	//draw_line(layer, rand()%96, rand()%48,
 	//rand()%96, rand()%48, &color);
 	//draw_line(layer, 0, 0,
@@ -51,9 +39,6 @@ int main(int argc, char **argv)
 	//draw_blinds(layer, 0, 0, 96, 0, 5, &color);
 	//draw_gradient(layer, 0, 0, &color, 48, 0, &color2);
         
-        i++;
-        i %= 48 * 24;
-
         /* Commit the layer */
         serverdata_commitlayer(s);
     }
