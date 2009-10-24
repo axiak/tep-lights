bd5c2b8bd64a1719a7e05be2cc5aaec88c596f06
kmill <kmill@c46fabcb-0fd2-43a4-8a37-601b2e6a48eb>
2009-10-24 00:50:14 +0000

    geomtest
    git-svn-id: svn+ssh://linerva.mit.edu/mit/axiak/svnrepos/public/tEp/cdmx@148 c46fabcb-0fd2-43a4-8a37-601b2e6a48eb

---
+++ plugins/geomtest.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <plugin.h>
+#include <geom.h>
+#include <ipcstructs.h>
+#include <dmx.h>
+
+#define PLUGINID 301
+
+int main(int argc, char **argv)
+{
+    int i, r, c, j;
+    LocalData * s = plugin_register(argv[0], PLUGINID);
+    ColorLayer * layer = s->layer;
+    ColorLayer * layer2;
+    RGBPixel color;
+    i = 0;
+    layer->width = 48;
+    layer->height = 24;
+    while (1) {
+        serverdata_update(s); /* Wait for audio info to update */
+
+        /* Get other plugin information */
+
+       /*  for (j = 0; j < MAXPLUGINS; j++) { */
+/*             if (s->info->input_plugins[j]) { */
+/*                 /\* The plugin is one of its inputs, we should use it now. *\/ */
+/*                 layer2 = plugin_useotherlayer(s->ipcdata, j); */
+/*                 /\* do stuff with layer2...*\/ */
+/*                 colorlayer_add(layer, layer2); */
+/*                 plugin_disuseotherlayer(s->ipcdata, j); */
+/*             } */
+/* 	} */
+
+
+        /* Do stuff to layer... */
+	rgbpixel_setvalue(&color, 1.0, 0, 1.0, 1.0);
+
+	colorlayer_setall(layer, 0, 0, 0, 0);
+	
+        r = i / 48;
+        c = i % 48;
+
+	draw_line(layer, r, c, 0, 0, &color);
+
+        i++;
+        i %= 48 * 24;
+
+        /* Commit the layer */
+        serverdata_commitlayer(s);
+    }
+
+    serverdata_destroy(s);
+    return 0;
+}
+
