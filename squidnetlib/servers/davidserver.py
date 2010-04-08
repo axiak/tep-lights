82e869977ab5ee2abc73dd05325c61079632ae60
axiak <axiak@7d6ba805-c9d4-4584-822b-803a16463d37>
2010-04-08 05:38:58 +0000

    Updated
    git-svn-id: svn+ssh://s0.mit.edu/home/subversion/squidnet@54 7d6ba805-c9d4-4584-822b-803a16463d37

--- squidnetlib/servers/davidserver.py
+++ squidnetlib/servers/davidserver.py
@@ -1,20 +1,57 @@
+#!/usr/bin/env python
 import os
 from squidnet import sexp, squidprotocol as sp, squidserver as ss, dmx
 
+import pydaemon
+
+pydaemon.createDaemon()
+
 sr = ss.ShellRunner()
 
-def quadsurf2(args):
-    prog = '/home/david/gorlack-code-computer/quadsurf-new2/graph'
-    sr.spawn(prog, [prog, args['brightness'].value])
+BASE_DIR = os.path.join(os.path.dirname(__file__), '..', '..', 'gorlack-code-computer')
+
+programs = {
+    'Quadsurf New 2': 'quadsurf-new2',
+    'Quadsurf New 1': 'quadsurf-new1',
+    'Hue Ball': 'hueball',
+    'Quadsurf': 'quadsurf',
+    'Blinky Cylon': 'blinkycylon',
+    'Strange': 'strange',
+    'Strobe Ball': 'strobeball',
+    'Eye of Gorlack': 'wtf-1',
+    'Paint Drip Up': 'paint-drip-up',
+}
+
+def viz(args):
+    sr.kill()
+    prog = os.path.join(BASE_DIR, programs[args['visualization'].value], 'graph')
+    base = prog
+    sr.spawn(prog, [base, str(args['brightness'].value)])
+
+def setcolor(args):
+    sr.kill()
+    prog = os.path.join(BASE_DIR, 'setcolor', 'graph')
+    color = [str(x / 256.0) for x in args['color'].value]
+    color.insert(0, prog)
+    sr.spawn(prog, color)
 
 serv = sp.SquidServer("david-xen", "david-xen.mit.edu", 2222, "David's computer")
 d1 = sp.SquidDevice("eye-of-gorlack", "The Eye of Gorlack")
 serv.add_device(d1)
-d1.add_message(sp.SquidMessage("quadsurf2",
-                               "Run quadsurf-new2",
-                               [sp.SquidArgument("brightness",
-                                                 sp.SquidRangeValue,
-                                                 sp.SquidRangeValue(0.8))],
-                               quadsurf2))
+d1.add_message(sp.SquidMessage("visualization",
+                               "Run visualizations",
+                               [
+            sp.SquidArgument("visualization",
+                             sp.SquidEnumFactory(*programs.keys())),
+            sp.SquidArgument("brightness",
+                             sp.SquidRangeValue,
+                             0.8)],
+                               viz))
+
+d1.add_message(sp.SquidMessage("set",
+                               'Set color',
+                               [sp.SquidArgument("color",
+                                                 sp.SquidColorValue, [230, 255, 179])],
+                               setcolor))
 
 ss.run_server(serv)
