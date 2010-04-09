de69970c513c267fe5dfc8d5f2e632f0858bf43b
root <root@7d6ba805-c9d4-4584-822b-803a16463d37>
2010-04-09 21:12:49 +0000

    update
    git-svn-id: svn+ssh://s0.mit.edu/home/subversion/squidnet@59 7d6ba805-c9d4-4584-822b-803a16463d37

--- squidnetlib/servers/subzeroserver.py
+++ squidnetlib/servers/subzeroserver.py
@@ -3,27 +3,33 @@ import os
 from squidnet import sexp, squidprotocol as sp, squidserver as ss, dmx
 
 wall_visualizations = {
-    'Shimmering': '/home/axiak/pydmx3/shimmering.py',
-    'Surf Ball': '/home/axiak/pydmx3/surfball.py',
-    'Flames': '/home/axiak/pydmx3/flames2.py',
-    'Conway\'s Game of Life': '/home/axiak/pydmx3/gol.py',
+    'Shimmering': 'shimmering',
+    'Surf Ball': 'surfball',
+    'Flames': 'flames2',
+    'Conway\'s Game of Life': 'gol',
 }
 
 wall_sr = ss.ShellRunner()
 
 def handle_wall_color(args):
-    prog = '/home/axiak/pydmx3/setcolor.py'
+    prog = '/home/axiak/pydmx5/setcolor.py'
     arg = [str(c / 255.0) for c in args['color'].value]
     arg.insert(0, prog)
     wall_sr.spawn(prog, arg)
     print prog, arg
 
 def handle_wall_viz(args):
-    prog = wall_visualizations[args['visualization'].value]
+    #prog = '/home/axiak/pydmx5/run_module.py'
+    viz = wall_visualizations[args['visualization'].value]
+    prog = '/home/axiak/pydmx5/%s.py' % viz
     wall_sr.spawn(prog)
 
+def handle_wall_image(args):
+    prog = '/home/axiak/pydmx5/setimage.py'
+    wall_sr.spawn(prog, [prog, args['Scaling'].value, args['image'].value])
 
-serv = sp.SquidServer("subzero", "subzero.mit.edu", 2222, "Subzero")
+
+serv = sp.SquidServer("subzero", "s0.mit.edu", 2222, "Subzero")
 
 d1 = sp.SquidDevice("dining-room-wall", "Dining room wall!")
 serv.add_device(d1)
@@ -38,5 +44,12 @@ d1.add_message(sp.SquidMessage("visualization",
                                [sp.SquidArgument("visualization", sp.SquidEnumFactory(*wall_visualizations))],
                                handle_wall_viz))
 
+
+d1.add_message(sp.SquidMessage("image",
+                               "Send an image",
+                               [sp.SquidArgument("Scaling", sp.SquidEnumFactory('resize', 'thumbnail')),
+                                sp.SquidArgument("image",
+                                                 sp.SquidBase64FileValue)],
+                               handle_wall_image))
 
 ss.run_server(serv)
