7bc1c11d7cf480296312a554d30ad15cb25f0310
Michael Axiak <axiak@mit.edu>
2010-04-13 02:55:58 -0400

    Added vertical binning

--- cdmx/plugins/sonogram-slow.c
+++ cdmx/plugins/sonogram-slow.c
@@ -8,35 +8,41 @@
 
 #define PLUGINID 103
 
-#define SLOWNESS 3
-
-enum {ever = 1};
+enum {ever = 1,
+      SLOWNESS = 3};
 
 void set_color_temperature(RGBPixel * target, float tempurature);
 
 
 int main(int argc, char **argv)
 {
-    int r;
+    int r, i, j;
     LocalData * s = plugin_register(argv[0], PLUGINID);
     ColorLayer * layer = s->layer;
     RGBPixel * cur;
     RGBPixel from_fft;
-    int i;
     int slowness = SLOWNESS;
-    int COLOR = 0;
+    int vertical_bin = 1;
+    int vertical_offset = 0;
     double max_avg, sum_avg;
-
+    int bins;
     CircleBuf * vals, *last;
 
     switch (argc) {
+    case 4:
+        vertical_offset = atoi(argv[3]);
     case 3:
-        COLOR = atoi(argv[2]);
+        vertical_bin = atoi(argv[2]);
     case 2:
         slowness = atoi(argv[1]);
         break;
     }
-    last = vals = circlebuf_create(layer->height,
+
+    vertical_offset += 2;
+    bins = vertical_bin * layer->height;
+
+
+    last = vals = circlebuf_create(bins,
                                    layer->width * slowness);
 
     while (last->prev != NULL) {
@@ -47,48 +53,31 @@ int main(int argc, char **argv)
         serverdata_update(s); /* Wait for audio info to update */
 
         max_avg = sum_avg = 0;
-        for (r = 0; r < layer->height; r++) {
-            if (s->soundinfo->short_avg[r] > max_avg)
-                max_avg = s->soundinfo->long_avg[r];
-            sum_avg += s->soundinfo->long_avg[r];
+        for (r = 0; r < bins; r++) {
+            if (s->soundinfo->long_avg[r + vertical_offset] > max_avg)
+                max_avg = s->soundinfo->long_avg[r + vertical_offset];
+            sum_avg += s->soundinfo->long_avg[r + vertical_offset];
         }
+        sum_avg /= bins;
 
-        for (r = 0; r < layer->height; r++) {
-            last->data[r] = pow(s->soundinfo->fft[r] / sum_avg * layer->height / 4, 0.7);
+        for (r = 0; r < bins; r++) {
+            last->data[r] = pow(s->soundinfo->fft[r + vertical_offset] / sum_avg / 4, 2);
         }
-        printf("%0.2f, %0.2f\n", last->data[0], last->data[14]);
         last->prev = vals;
         vals = last;
         colorlayer_setall(layer, 0, 0, 0, 1);
 
         for (i = 0; last->prev != vals; i++, last = last->prev) {
-            for (r = 0; r < layer->height; r++) {
-
-                set_color_temperature(&from_fft, last->data[r] / (float)slowness);
-                cur = colorlayer_getpixel(layer, layer->width - i / slowness - 1, r);
-                rgbpixel_setvalue(cur,
-                                  cur->red + from_fft.red,
-                                  cur->green + from_fft.green,
-                                  cur->blue + from_fft.blue,
-                                  1);
-                /*
-                if (i > slowness) {
-                    cur = colorlayer_getpixel(layer, layer->width - i / slowness, r);
+            for (r = 0; r < layer->height; ++r) {
+                for (j = 0; j < vertical_bin; j++) {
+                    set_color_temperature(&from_fft, last->data[vertical_bin * r + j] / (float)slowness / (float)vertical_bin);
+                    cur = colorlayer_getpixel(layer, layer->width - i / slowness - 1, r);
                     rgbpixel_setvalue(cur,
-                                      cur->red + 0.25 * from_fft.red,
-                                      cur->green + 0.25 * from_fft.green,
-                                      cur->blue + 0.25 * from_fft.blue,
+                                      cur->red + from_fft.red,
+                                      cur->green + from_fft.green,
+                                      cur->blue + from_fft.blue,
                                       1);
                 }
-                if (i < (layer->height - slowness)) {
-                    cur = colorlayer_getpixel(layer, layer->width - i / slowness - 2, r);
-                    rgbpixel_setvalue(cur,
-                                      cur->red + 0.25 * from_fft.red,
-                                      cur->green + 0.25 * from_fft.green,
-                                      cur->blue + 0.25 * from_fft.blue,
-                                      1);
-                                      }*/
-           
             }
         }
 
@@ -102,12 +91,6 @@ int main(int argc, char **argv)
 }
 
 
-/*
-typedef struct CircleBuf_s {
-    struct CircleBuf_s * pref;
-    double * data;
-} CircleBuf;
-*/
 
 CircleBuf * circlebuf_create(int rows, int cols)
 {
